87ac64355da466a9d418aba763d7e6f735fde997
Nicolas Vidoni <nicolas.vidoni@sbb.ch>
2022-04-12 18:02:08 +0200

    rename MyWebApp -> WebApp

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,4 +4,4 @@ import 'package:flutter/material.dart';
 import 'native_app.dart';
 import 'web_app.dart';
 
-void main() => runApp(kIsWeb ? MyWebApp() : MyApp());
+void main() => runApp(kIsWeb ? WebApp() : MyApp());
