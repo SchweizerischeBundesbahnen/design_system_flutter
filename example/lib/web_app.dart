40dab6c38779d7964f6e613e1326c8ba5a84c825
Meyer Cyrill (IT-OM-KB-RSU) <cyrill.meyer@sbb.ch>
2022-05-09 10:06:44 +0200

    finished sidebar navigation example

--- example/lib/web_app.dart
+++ example/lib/web_app.dart
@@ -1,20 +1,112 @@
 import 'package:design_system_flutter/design_system_flutter.dart';
 import 'package:flutter/material.dart';
 
-import 'webnav/nav.dart';
+import 'web_pages/web_pages.dart';
+
+const _pages = {
+  'Logo': LogoPage(),
+  'Buttons': ButtonPage(),
+  'Icons': IconPage(),
+  'Breadcrumb': BreadcrumbPage(),
+  'Loading Indicator': LoadingPage(),
+};
+
+String _nameToRoute(String name) =>
+    '/${name.toLowerCase().replaceAll(' ', '-')}';
 
 class WebApp extends StatelessWidget {
-  WebApp({Key? key}) : super(key: key);
-
-  final _appRouter = AppRouter();
+  const WebApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerDelegate: _appRouter.delegate(),
-      routeInformationParser: _appRouter.defaultRouteParser(),
+    return MaterialApp(
       theme: SBBTheme.light(),
       darkTheme: SBBTheme.dark(),
+      themeMode: ThemeMode.light,
+      initialRoute: _nameToRoute(_pages.keys.first),
+      onGenerateRoute: (settings) => PageRouteBuilder(
+        settings: settings,
+        pageBuilder: (_, __, ___) {
+          for (final pageName in _pages.keys) {
+            if (_nameToRoute(pageName) == settings.name) {
+              return _sidePanelPageByName(pageName);
+            }
+          }
+          return _sidePanelPageByName(_pages.keys.first);
+        },
+      ),
     );
   }
 }
+
+Widget _sidePanelPageByName(String name) {
+  return _SidePanelPage(
+    name: name,
+    page: _pages[name]!,
+  );
+}
+
+class _SidePanelPage extends StatelessWidget {
+  const _SidePanelPage({
+    Key? key,
+    required this.name,
+    required this.page,
+  }) : super(key: key);
+
+  final String name;
+  final Widget page;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: SBBWebHeader(
+        title: 'Example Web App',
+        subtitle: 'Version 0.1',
+        userMenu: _exampleUserMenu,
+      ),
+      body: SBBSidebar(
+        body: page,
+        items: _pages.keys
+            .map((name) => _buildSidebarItem(context, name))
+            .toList(),
+      ),
+    );
+  }
+
+  Widget _buildSidebarItem(BuildContext context, String name) {
+    final route = _nameToRoute(name);
+    return SBBSidebarItem(
+      title: name,
+      onTap: () {
+        Navigator.of(context).pushNamed(route);
+      },
+      isSelected: ModalRoute.of(context)?.settings.name == route,
+    );
+  }
+}
+
+final SBBUserMenu _exampleUserMenu = SBBUserMenu(
+  displayName: "Eisen Bahner",
+  itemBuilder: (BuildContext context) => <SBBMenuEntry>[
+    SBBMenuItem.tile(
+      icon: SBBIcons.user_medium,
+      title: 'Benutzer',
+    ),
+    SBBMenuItem.tile(
+      icon: SBBIcons.app_icon_medium,
+      title: 'Über',
+    ),
+    SBBMenuItem.tile(
+      icon: SBBIcons.gears_medium,
+      title: 'Einstellungen',
+    ),
+    SBBMenuDivider(),
+    SBBMenuItem.tile(
+      icon: SBBIcons.exit_medium,
+      title: 'Logout',
+    ),
+  ],
+  onLoginRequest: () {
+    print('Login!');
+  },
+);
---
+++ example/lib/web_pages/web_pages.dart
@@ -0,0 +1,5 @@
+export 'breadcrumb_page.dart';
+export 'button_page.dart';
+export 'icon_page.dart';
+export 'loading_page.dart';
+export 'logo_page.dart';
