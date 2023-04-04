443ce191d84e2dee6ab001592c8f87ab4258b736
Dominik Mosberger <9129636+mosberger@users.noreply.github.com>
2023-04-04 11:12:56 +0200

    (#52) change segmented button colors to meet WCAG 2.1 requirements

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -1,14 +1,11 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 import 'test_app.dart';
 
-Future<void> testExecutable(FutureOr<void> Function() testMain) async {
-  final skipGoldenAssertion = Platform.operatingSystem != 'macos' || !Platform.operatingSystemVersion.contains('12');
-  return GoldenToolkit.runWithConfiguration(
-    () => TestApp.setupAll().then((value) => testMain()),
-    config: GoldenToolkitConfiguration(skipGoldenAssertion: () => skipGoldenAssertion),
-  );
-}
+Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
+    GoldenToolkit.runWithConfiguration(
+      () => TestApp.setupAll().then((value) => testMain()),
+      config: GoldenToolkitConfiguration(skipGoldenAssertion: () => false),
+    );
