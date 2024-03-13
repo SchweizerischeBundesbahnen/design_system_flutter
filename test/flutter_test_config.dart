d43951d0611e7b33c47f2c080aba28b16588ab79
u221638 <ralf.winkelmann2@sbb.ch>
2024-03-13 10:18:36 +0100

    Fixed test config

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -9,7 +9,7 @@ import 'test_app.dart';
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   final testUrl = (goldenFileComparator as LocalFileComparator).basedir;
   goldenFileComparator = GoldenDiffComparator('$testUrl/test.dart');
-  GoldenToolkit.runWithConfiguration(
+  return GoldenToolkit.runWithConfiguration(
         () => TestApp.setupAll().then((value) => testMain()),
     config: GoldenToolkitConfiguration(skipGoldenAssertion: () => false),
   );
