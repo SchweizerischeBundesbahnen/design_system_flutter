ec9f9c6f9711115e25cf39ed000d54fb4f851757
Hoang Tran <3406885+VanHoangTran@users.noreply.github.com>
2024-07-03 20:09:12 +0200

    test: add golden tests for picker input fields

---
+++ test/sbb_date_input_test.dart
@@ -0,0 +1,99 @@
+import 'package:design_system_flutter/design_system_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
+
+import 'test_app.dart';
+
+void main() {
+  void generateTest(String name) {
+    testWidgets(name, (WidgetTester tester) async {
+      await Specs.run(
+        Specs.mobileSpecs,
+        const DatePickerTest(),
+        tester,
+        name,
+        find.byType(DatePickerTest),
+      );
+    });
+  }
+
+  generateTest('date_input_test_1');
+}
+
+class DatePickerTest extends StatelessWidget {
+  const DatePickerTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final initialDate = DateTime(2024, 07, 03);
+    final minimumDate = DateTime(2024, 08, 01);
+    final maximumDate = DateTime(2024, 07, 01);
+    return SBBGroup(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          SBBDateInput(
+            onDateChanged: (_) {},
+          ),
+          SBBDateInput(
+            value: initialDate,
+            onDateChanged: (_) {},
+          ),
+          SBBDateInput(
+            hintText: 'Hint only',
+            onDateChanged: (_) {},
+          ),
+          SBBDateInput(
+            labelText: 'Label and Hint',
+            hintText: 'Label and Hint',
+            onDateChanged: (_) {},
+          ),
+          SBBDateInput(
+            labelText: 'Label and Value',
+            value: initialDate,
+            onDateChanged: (_) {},
+          ),
+          SBBDateInput(
+            value: initialDate,
+            labelText: 'Custom date format',
+            dateFormat: DateFormat('dd.MM.yy'),
+            onDateChanged: (_) {},
+          ),
+          SBBDateInput(
+            value: initialDate,
+            labelText: 'Error',
+            errorText: 'Error',
+            prefixIcon: SBBIcons.calendar_small,
+            suffixIcon: SBBIcons.arrow_circle_reset_small,
+            onDateChanged: (_) {},
+          ),
+          SBBDateInput(
+            value: initialDate,
+            labelText: 'Disabled',
+            prefixIcon: SBBIcons.calendar_small,
+            suffixIcon: SBBIcons.arrow_circle_reset_small,
+            onDateChanged: null,
+          ),
+          SBBDateInput(
+            value: initialDate,
+            minimumDate: minimumDate,
+            labelText: 'Initial date before minimum date',
+            prefixIcon: SBBIcons.calendar_small,
+            suffixIcon: SBBIcons.arrow_circle_reset_small,
+            onDateChanged: (_) {},
+          ),
+          SBBDateInput(
+            value: initialDate,
+            maximumDate: maximumDate,
+            labelText: 'Initial date after maximum date',
+            prefixIcon: SBBIcons.calendar_small,
+            suffixIcon: SBBIcons.arrow_circle_reset_small,
+            onDateChanged: (_) {},
+            isLastElement: true,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ test/sbb_date_time_input_test.dart
@@ -0,0 +1,107 @@
+import 'package:design_system_flutter/design_system_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
+
+import 'test_app.dart';
+
+void main() {
+  void generateTest(String name) {
+    testWidgets(name, (WidgetTester tester) async {
+      await Specs.run(
+        Specs.mobileSpecs,
+        const DateTimePickerTest(),
+        tester,
+        name,
+        find.byType(DateTimePickerTest),
+      );
+    });
+  }
+
+  generateTest('date_time_input_test_1');
+}
+
+class DateTimePickerTest extends StatelessWidget {
+  const DateTimePickerTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final initialDateTime = DateTime(2024, 07, 03, 12, 33);
+    final minimumDateTime = DateTime(2024, 08, 01, 16, 12);
+    final maximumDateTime = DateTime(2024, 07, 01, 08, 43);
+    return SBBGroup(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          SBBDateTimeInput(
+            onDateTimeChanged: (_) {},
+          ),
+          SBBDateTimeInput(
+            value: initialDateTime,
+            onDateTimeChanged: (_) {},
+          ),
+          SBBDateTimeInput(
+            hintText: 'Hint only',
+            onDateTimeChanged: (_) {},
+          ),
+          SBBDateTimeInput(
+            labelText: 'Label and Hint',
+            hintText: 'Label and Hint',
+            onDateTimeChanged: (_) {},
+          ),
+          SBBDateTimeInput(
+            labelText: 'Label and Value',
+            value: initialDateTime,
+            onDateTimeChanged: (_) {},
+          ),
+          SBBDateTimeInput(
+            value: initialDateTime,
+            labelText: 'Custom date format',
+            dateFormat: DateFormat('dd.MM.yy, H:m'),
+            onDateTimeChanged: (_) {},
+          ),
+          SBBDateTimeInput(
+            value: initialDateTime,
+            labelText: 'Error',
+            errorText: 'Error',
+            prefixIcon: SBBIcons.calendar_small,
+            suffixIcon: SBBIcons.arrow_circle_reset_small,
+            onDateTimeChanged: (_) {},
+          ),
+          SBBDateTimeInput(
+            value: initialDateTime,
+            labelText: 'Disabled',
+            prefixIcon: SBBIcons.calendar_small,
+            suffixIcon: SBBIcons.arrow_circle_reset_small,
+            onDateTimeChanged: null,
+          ),
+          SBBDateTimeInput(
+            value: initialDateTime,
+            labelText: 'Minute interval',
+            minuteInterval: 15,
+            prefixIcon: SBBIcons.calendar_small,
+            suffixIcon: SBBIcons.arrow_circle_reset_small,
+            onDateTimeChanged: (_) {},
+          ),
+          SBBDateTimeInput(
+            value: initialDateTime,
+            minimumDateTime: minimumDateTime,
+            labelText: 'Initial date before minimum date',
+            prefixIcon: SBBIcons.calendar_small,
+            suffixIcon: SBBIcons.arrow_circle_reset_small,
+            onDateTimeChanged: (_) {},
+          ),
+          SBBDateTimeInput(
+            value: initialDateTime,
+            maximumDateTime: maximumDateTime,
+            labelText: 'Initial date after maximum date',
+            prefixIcon: SBBIcons.calendar_small,
+            suffixIcon: SBBIcons.arrow_circle_reset_small,
+            onDateTimeChanged: (_) {},
+            isLastElement: true,
+          ),
+        ],
+      ),
+    );
+  }
+}
