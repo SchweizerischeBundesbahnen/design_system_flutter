1052eeab0c8a656f21dec1d969762012ba6c1cd5
Nico Vidoni <nicolas.vidoni@sbb.ch>
2024-12-11 16:57:26 +0100

    tests: add golden tests for all buttons

---
+++ test/sbb_tertiary_button_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
+
+import 'test_app.dart';
+
+void main() {
+  testWidgets('tertiary_button', (WidgetTester tester) async {
+    final widget = Padding(
+      padding: const EdgeInsets.symmetric(horizontal: sbbDefaultSpacing / 2),
+      child: Column(
+        children: [
+          const SizedBox(height: sbbDefaultSpacing),
+          SBBTertiaryButtonLarge(label: "Default", onPressed: () {}),
+          const SizedBox(height: sbbDefaultSpacing),
+          SBBTertiaryButtonLarge(label: "Disabled", onPressed: null),
+          const SizedBox(height: sbbDefaultSpacing),
+          SBBTertiaryButtonLarge(label: "Icon", onPressed: () {}, icon: SBBIcons.dog_medium),
+          const SizedBox(height: sbbDefaultSpacing),
+          SBBTertiaryButtonLarge(label: "Icon Disabled", onPressed: null, icon: SBBIcons.dog_medium),
+          const SizedBox(height: sbbDefaultSpacing),
+          SBBTertiaryButtonSmall(label: "Default", onPressed: () {}),
+          const SizedBox(height: sbbDefaultSpacing),
+          SBBTertiaryButtonSmall(label: "Disabled", onPressed: null),
+          const SizedBox(height: sbbDefaultSpacing),
+          SBBTertiaryButtonSmall(label: "Icon", onPressed: () {}, icon: SBBIcons.dog_small),
+          const SizedBox(height: sbbDefaultSpacing),
+          SBBTertiaryButtonSmall(label: "Icon Disabled", onPressed: null, icon: SBBIcons.dog_small),
+        ],
+      ),
+    );
+    await TestSpecs.run(
+      TestSpecs.themedSpecs,
+      widget,
+      tester,
+      'tertiary_button',
+      find.byType(Column).first,
+    );
+  });
+}
