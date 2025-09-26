a8dcd5f892ee70b79ceddb41e00bebd68a6d0562
Nico Vidoni <30844036+smallTrogdor@users.noreply.github.com>
2025-09-26 16:28:17 +0200

    feat: use functional colors in SBBStatus and SBBNotificationBox (#388)
    * feat: use functional colors in SBBStatus and SBBNotificationBox
    
    * test: update tests to new specs

--- lib/src/status/sbb_status.type.dart
+++ lib/src/status/sbb_status.type.dart
@@ -1,14 +1,28 @@
 part of 'sbb_status.dart';
 
 enum SBBStatusType {
-  alert(SBBColors.red, SBBColors.white, SBBIcons.circle_cross_small),
-  warning(SBBColors.peach, SBBColors.black, SBBIcons.circle_exclamation_point_small),
-  success(SBBColors.green, SBBColors.white, SBBIcons.circle_tick_small),
-  information(SBBColors.smoke, SBBColors.white, SBBIcons.circle_information_small);
+  alert(SBBColors.error, SBBColors.errorDark, SBBColors.white, SBBColors.white, SBBIcons.circle_cross_small),
+  warning(
+    SBBColors.warning,
+    SBBColors.warningDark,
+    SBBColors.black,
+    SBBColors.white,
+    SBBIcons.circle_exclamation_point_small,
+  ),
+  success(SBBColors.success, SBBColors.successDark, SBBColors.white, SBBColors.white, SBBIcons.circle_tick_small),
+  information(
+    SBBColors.smoke,
+    SBBColors.anthracite,
+    SBBColors.white,
+    SBBColors.white,
+    SBBIcons.circle_information_small,
+  );
 
-  const SBBStatusType(this.backgroundColor, this.iconColor, this.icon);
+  const SBBStatusType(this.backgroundColor, this.backgroundColorDark, this.iconColor, this.iconColorDark, this.icon);
 
   final Color backgroundColor;
+  final Color backgroundColorDark;
   final Color iconColor;
+  final Color iconColorDark;
   final IconData icon;
 }
