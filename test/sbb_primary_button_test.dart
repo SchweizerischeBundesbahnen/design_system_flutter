c8ea909d7de8afd1e8b58cbda80c412326c83d63
Nico Vidoni <nicolas.vidoni@gmx.net>
2025-11-18 08:44:49 +0100

    test: adapt golden tests for small pixel changes with buttons

--- test/sbb_primary_button_test.dart
+++ test/sbb_primary_button_test.dart
@@ -6,12 +6,13 @@ import 'test_app.dart';
 
 void main() {
   testWidgets('primary_button', (WidgetTester tester) async {
+    final pressableKey = ValueKey('pressedPrimaryButton');
     final widget = Padding(
       padding: const EdgeInsets.symmetric(horizontal: sbbDefaultSpacing / 2, vertical: sbbDefaultSpacing),
       child: Column(
         spacing: sbbDefaultSpacing,
         children: [
-          SBBPrimaryButton(labelText: "Default", onPressed: () {}),
+          SBBPrimaryButton(key: pressableKey, labelText: "Default", onPressed: () {}),
           SBBPrimaryButton(labelText: "Disabled", onPressed: null),
           SBBPrimaryButton(
             label: Container(color: SBBColors.platinum, child: Text('Custom!')),
@@ -25,6 +26,16 @@ void main() {
       widget,
       tester,
       'primary_button',
+      find.byType(Column).first,
+    );
+    await tester.press(find.byKey(pressableKey));
+    await tester.pumpAndSettle();
+
+    await TestSpecs.run(
+      TestSpecs.themedSpecs,
+      widget,
+      tester,
+      'primary_button_pressed',
       find.byType(Column).first,
     );
   });
