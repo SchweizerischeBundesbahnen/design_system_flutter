d64f3f83822a5183f24a0c16a2e4ac3a30fdfe6f
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-01-29 15:19:05 +0100

    test: rework slider tests

--- test/sbb_slider_test.dart
+++ test/sbb_slider_test.dart
@@ -5,30 +5,42 @@ import 'package:sbb_design_system_mobile/sbb_design_system_mobile.dart';
 import 'test_app.dart';
 
 void main() {
-  testWidgets('slider_1', (WidgetTester tester) async {
-    final widget = Column(
-      children: [
-        SBBSlider(onChanged: (value) {}, value: 50, max: 100),
-        const SizedBox(height: SBBSpacing.medium),
-        SBBSlider(
-          onChanged: (value) {},
-          value: 25,
-          max: 100,
-          leadingIconData: null,
-          trailingIconData: null,
-          onChangeStart: (value) {},
-          onChangeEnd: (value) {},
-        ),
-        const SizedBox(height: SBBSpacing.medium),
-        const SBBSlider(onChanged: null, value: 75, max: 100),
-      ],
+  testWidgets('slider', (WidgetTester tester) async {
+    final widget = Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: SBBSpacing.medium,
+        children: [
+          SBBSlider(onChanged: (value) {}, value: 50, max: 100),
+          SBBSlider(
+            onChanged: (_) {},
+            value: 25,
+            max: 100,
+            leadingIconData: SBBIcons.walk_slow_small,
+            trailingIconData: SBBIcons.walk_fast_small,
+          ),
+          const SBBSlider(
+            onChanged: null,
+            value: 75,
+            max: 100,
+            leadingIconData: SBBIcons.walk_slow_small,
+            trailingIconData: SBBIcons.walk_fast_small,
+          ),
+          SBBSlider(
+            onChanged: (_) {},
+            value: 75,
+            max: 100,
+            leading: Container(color: SBBColors.green, width: 10, height: 10),
+          ),
+        ],
+      ),
     );
 
     await TestSpecs.run(
       TestSpecs.themedSpecs,
       widget,
       tester,
-      'slider_1',
+      'slider',
       find.byType(Column).first,
     );
   });
