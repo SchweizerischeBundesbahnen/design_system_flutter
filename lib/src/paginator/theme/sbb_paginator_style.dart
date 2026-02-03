39b5f00ad8889fcc7ba7bc508c08b9f92d75b7ec
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-02-03 17:57:08 +0100

    feat: add SBBPaginatorThemeData and DefaultSBBPaginatorThemeData

--- lib/src/paginator/theme/sbb_paginator_style.dart
+++ lib/src/paginator/theme/sbb_paginator_style.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../theme.dart';
+import '../../theme/theme.dart';
 
 class SBBPaginatorStyle {
   SBBPaginatorStyle({this.selectedColor, this.borderColor, this.floatingBackgroundColor});
--- lib/src/theme/styles/styles.dart
+++ lib/src/theme/styles/styles.dart
@@ -1,12 +1,12 @@
 export '../../button/theme/sbb_button_style.dart';
 export '../../container/theme/sbb_content_box_style.dart';
+export '../../paginator/theme/sbb_paginator_style.dart';
 export 'src/promotion_box_style.dart';
 export 'src/sbb_base_style.dart';
 export 'src/sbb_basic_control_style.dart';
 export 'src/sbb_control_style.dart';
 export 'src/sbb_control_styles.dart';
 export 'src/sbb_headerbox_style.dart';
-export 'src/sbb_paginator_style.dart';
 export 'src/sbb_picker_style.dart';
 export 'src/sbb_segmented_button_inner_style.dart';
 export 'src/sbb_segmented_button_style.dart';
