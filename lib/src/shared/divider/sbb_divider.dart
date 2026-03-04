63950f92d9c08110c264f62b36fad05180cbff68
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-03-04 12:36:52 +0100

    docs: add documentation

--- lib/src/shared/divider/sbb_divider.dart
+++ lib/src/shared/divider/sbb_divider.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:sbb_design_system_mobile/src/shared/divider/divider_painter.dart';
 
+/// A one-pixel divider line using the SBB design system styling.
+///
+/// See also:
+///  * [SBBListItem.divideListItems], for automatically adding dividers between list items.
 class SBBDivider extends StatelessWidget {
   const SBBDivider({super.key, this.color});
 
+  /// The color of the divider line.
+  ///
+  /// If null, defaults to [ThemeData.dividerColor].
   final Color? color;
 
   @override
