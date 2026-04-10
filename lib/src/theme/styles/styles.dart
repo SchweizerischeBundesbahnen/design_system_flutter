98ab5de960f75307102bbc46cf49d10d92235820
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-04-10 16:44:58 +0200

    feat!: rm SBBTextFieldStyle

--- lib/src/theme/styles/styles.dart
+++ lib/src/theme/styles/styles.dart
@@ -5,4 +5,3 @@ export 'src/sbb_base_style.dart';
 export 'src/sbb_control_styles.dart';
 export 'src/sbb_headerbox_style.dart';
 export 'src/sbb_picker_style.dart';
-export 'src/sbb_text_field_style.dart';
