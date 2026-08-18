b64fade5fa27edb0d3bd4985e8f51d24c4c1543c
Nico Vidoni <nicolas.vidoni@gmx.net>
2026-08-18 17:28:50 +0200

    test: add toast test

--- example/integration_test/app_test.dart
+++ example/integration_test/app_test.dart
@@ -12,6 +12,7 @@ import 'segmented_button_test.dart' as segmented_button_test;
 import 'slide_to_toggle_test.dart' as slide_to_toggle_test;
 import 'stepper_test.dart' as stepper_test;
 import 'tab_bar_test.dart' as tab_bar_test;
+import 'toast_test.dart' as toast_test;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -33,4 +34,5 @@ void main() {
   segmented_button_test.main();
   slide_to_toggle_test.main();
   stepper_test.main();
+  toast_test.main();
 }
